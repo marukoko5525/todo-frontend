68181c9d8cfd7e642ac7a4f2317a81e5434001c0
marukoko5525 <augusutynice@gmail.com>
2026-06-29 23:23:25 +0900

    add VITE_API_URL support

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { Task, TaskRequest } from '../types/task'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL ?? '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
